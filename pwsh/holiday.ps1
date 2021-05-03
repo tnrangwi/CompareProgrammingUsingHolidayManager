e79596738d98bbf7f050fa25e2bd28628aeb28b1
Thorsten <trtoaster@googlemail.com>
2021-05-03 22:44:14 +0200

    Push config reader into module

--- pwsh/holiday.ps1
+++ pwsh/holiday.ps1
@@ -1,60 +1,7 @@
 #!/usr/local/bin/pwsh
 $ErrorActionPreference="Stop"
-
-function Read-ConfigFile ([String]$fn) {
-    #FIXME: Error handling not there at all.
-    $cfg = @{}
-    $section = $null
-    $secNr = 0
-    #switch -f <file> would do as well
-    Get-Content $fn | ForEach-Object {
-        Switch -regex  ($_) {
-            "^#" {
-                #ignore comments
-                Break
-            } "^\[([a-zA-Z]+)\]" {
-                #new section
-                $section = $matches[1]
-                if ($cfg.ContainsKey($section)) {
-                    $cfg[$section] += @{}
-                    $secNr = $cfg[$section].Length - 1
-                } else {
-                    $cfg.Add($section, @(@{}))
-                    $secNr = 0
-                }
-                Break
-            } '^([a-zA-Z][a-zA-Z0-9_]*)=(.*)$' {
-                $key = $matches[1]
-                $valstr = $matches[2]
-                if ($section -eq $null) {
-                    $section = "default"
-                    $cfg.Add($section, @(@{}))
-                }
-                if ($valstr -match '^-?[0-9]{1,9}$') {
-                    $val = $valstr -as [int]
-                } elseif ($valstr -match '^+?[0-9]{1,9}[.]?[0-9]{0,9}' -or $valstr -match '^-?[0-9]{1,9}[.]?[0-9]{0,9}E-?[0-9]{1,3}') {
-                    $val = $valstr -as [float]
-                } elseif ($valstr -match '^"(.*)"$') {
-                    $val = $matches[1]
-                } elseif ($valstr.Length -eq 0) {
-                    $val = $null
-                } else {
-                    Write-Host "Parser error - unsupported value type"
-                    return $null
-                }
-                if ($cfg.$section[$secNr].ContainsKey($key)) {
-                    $cfg.$section[$secNr].$key += $val
-                } else {
-                    $cfg.$section[$secNr].Add($key, @($val))
-                }
-                Break
-            } default {
-                throw "Parser error - unsupported line in config file:'" + $switch.Current + "'"
-            }
-        }
-    }
-    $cfg
-}
+$Env:PSModulePath="${Env:PSModulePath}:$PSScriptRoot/lib"
+Import-Module ConfigReader
 
 $cfgFile = "${PSScriptRoot}/etc/holiday.conf"
 if (Test-Path $cfgFile) {
---
+++ pwsh/lib/ConfigReader/ConfigReader.psd1
@@ -0,0 +1,11 @@
+@{
+  RootModule = 'ConfigReader.psm1'
+  ModuleVersion = '1.0'
+  CompatiblePSEditions = @('Core')
+  Author = 'Thorsten Rangwich'
+  FunctionsToExport = @('Read-ConfigFile')
+  CmdletsToExport = @()
+  VariablesToExport = @()
+  AliasesToExport = @()
+  DscResourcesToExport = @()
+}
